7cb3f13578115d9cdd3f0c5eeb674f35825fc813
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2014-10-20 01:53:12 +0200

    initial commit: rails-3.2.19 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schwarzburgplattform::Application.config.secret_token = '4d5e52a9d589f628236772ed926c0a269b539c180aff4e61df8a48355f358fd6fc03c94285e8aaa0c5a69b3117fa3d171624f98bbdd78e99722b8ba467c338d6'
